06380bcc97fcfeb903c7bfab664469515d626e14
adamkt09 <90269516+adamkt09@users.noreply.github.com>
2023-04-25 07:30:58 -0500

    Add files via upload

---
+++ image gallery/image-gallery.py
@@ -0,0 +1,10 @@
+from PIL import Image
+
+image = Image.open("images/christmas1.png")
+image.show()
+
+image = Image.open("images/xmas2.png")
+image.show()
+
+image = Image.open("images/xmas3.png")
+image.show()
